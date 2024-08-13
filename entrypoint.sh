815c8b4a6b59110dc283da8ca7fa6247f2a7121f
Volodymyr Atamanenko <v@atamanenko.in>
2024-08-13 22:58:48 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ echo "Starting Jekyll build"
 ####################################################
 
 chmod -R a+w /github/workspace
-
+chown -R $(whoami):$(whoami) /github/workspace
 sh -c "git config --global --add safe.directory $PWD"
 sh -c "git config --global --add safe.directory /github/workspace"
 
